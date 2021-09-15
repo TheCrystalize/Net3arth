358e934c64fa08164908beed1f31caf2c236df47
Edward Haas <efhiii2000@gmail.com>
2021-09-15 11:20:05 -0700

    fixed syntax

--- nodes/hypertile3.js
+++ nodes/hypertile3.js
@@ -4,65 +4,75 @@ function hypertile3(p, q, r, shift) {
     a = Math.asinh(Math.sin(Math.PI / q) / Math.sin(Math.PI / p) * Math.sinh(o)),
     b = Math.asinh(Math.sin(Math.PI / r) * Math.sinh(a)),
     c = Math.asinh(Math.sin(Math.PI / r) * Math.sinh(a) / Math.sin(Math.PI / q)),
-    h = Math.tanh(b / 2.0),
-    b1 = Math.tanh(Math.acosh(Math.cosh(c) / Math.cosh(b)) / 2.0), b2 = Math.tanh(Math.acosh(Math.cosh(a) / Math.cosh(b)) / 2.0),
-    rot1 = 360 / p * rad, rot2 = 360 / q * rad, rot3 = 360 / r * rad,
-    r01 = C(Math.cos(rot1), Math.sin(rot1)), r02 = C(Math.cos(rot2), Math.sin(rot2), r03 = C(Math.cos(rot3), Math.sin(rot3));
+    h = Math.tanh(b / 2),
+    b1 = Math.tanh(Math.acosh(Math.cosh(c) / Math.cosh(b)) / 2),
+    b2 = Math.tanh(Math.acosh(Math.cosh(a) / Math.cosh(b)) / 2),
+    rot1 = 360 / p * rad,
+    rot2 = 360 / q * rad,
+    rot3 = 360 / r * rad,
+    r01 = C(Math.cos(rot1), Math.sin(rot1)),
+    r02 = C(Math.cos(rot2), Math.sin(rot2)),
+    r03 = C(Math.cos(rot3), Math.sin(rot3));
+
+  const c1 = C(h, 0),
+    c2 = C(0, b1),
+    c3 = C(0, b2);
+
   return function(z) {
-    let c1 = C(h, 0), c2 = C(0, b1), c3 = C(0, b2);
+    let n, prf, z0;
     if(shift < 0.25) {
-      let n = 0.0
-        pfr = 0.0
-        z0 = z
-    } else if (shift < 0.5) {
-      let n = p
-        pfr = rot1
-        z0 = div(add(z, c1), add(mult(c1, z), 1.0))
-    } else if (shift < 0.75) {
-      let n = q
-        pfr = rot2
-        z0 = div(add(z, -c2), add(mult(c2, z), 1.0))
+      n = 0
+      pfr = 0
+      z0 = z
+    } else if(shift < 0.5) {
+      n = p
+      pfr = rot1
+      z0 = div(add(z, c1), add(mult(c1, z), 1))
+    } else if(shift < 0.75) {
+      n = q
+      pfr = rot2
+      z0 = div(add(z, -c2), add(mult(c2, z), 1))
     } else {
-      let n = r
-        pfr = rot3
-        z0 = div(add(z, c3), add(mult(-c3, z), 1.0))
-    };
-    let m0 = div(add(z0, -c1), add(mult(-c1, z0) 1.0)),
+      n = r
+      pfr = rot3
+      z0 = div(add(z, c3), add(mult(-c3, z), 1))
+    }
+
+    let m0 = div(add(z0, -c1), add(mult(-c1, z0), 1)),
       r1 = r01 * m0,
-      m0f = div(add(r1, c1), add(mult(c1, r1) 1.0)),
-
-      m1 = div(add(z0, c2), add(mult(-c2, z0) 1.0)),
+      m0f = div(add(r1, c1), add(mult(c1, r1), 1)),
+      m1 = div(add(z0, c2), add(mult(-c2, z0), 1)),
       r2 = r02 * m1,
-      m1f = div(add(r2, -c2), add(mult(c2, r2) 1.0)),
+      m1f = div(add(r2, -c2), add(mult(c2, r2), 1)),
 
-      m2 = div(add(z0, -c3), add(mult(c3, z0) 1.0)),
+      m2 = div(add(z0, -c3), add(mult(c3, z0), 1)),
       r3 = r03 * m2,
-      m2f = div(add(r3, c3), add(mult(-c3, r3) 1.0)),
+      m2f = div(add(r3, c3), add(mult(-c3, r3), 1)),
 
       fr = Math.floor(Math.random() * n) * pfr,
-      rnd = Math.random(), f3, f0, f;
-      if(rnd < 1 / 3) {
-        f3 = m0f
-      } else if (rnd < 2 / 3) {
-        f3 = m1f
-      } else {
-        f3 = m2f
-      };
-      if(shift < 0.25) {
-        f0 = f3
-      } else if (shift < 0.5) {
-        f0 = div(add(f3, -c1), add(mult(-c1, f3) 1.0))
-      } else if (shift < 0.75) {
-        f0 = div(add(f3, c2), add(mult(-c2, f3) 1.0))
-      } else {
-        f0 = div(add(f3, -c3), add(mult(c3, f3) 1.0))
-      };
-      if(shift < 0.25) {
-        f = f0
-      } else {
-        f = C(Math.cos(fr), Math.sin(fr)) * f0
-      }
-      ;
+      rnd = Math.random(),
+      f3, f0, f;
+    if(rnd < 1 / 3) {
+      f3 = m0f
+    } else if(rnd < 2 / 3) {
+      f3 = m1f
+    } else {
+      f3 = m2f
+    };
+    if(shift < 0.25) {
+      f0 = f3
+    } else if(shift < 0.5) {
+      f0 = div(add(f3, -c1), add(mult(-c1, f3), 1))
+    } else if(shift < 0.75) {
+      f0 = div(add(f3, c2), add(mult(-c2, f3), 1))
+    } else {
+      f0 = div(add(f3, -c3), add(mult(c3, f3), 1))
+    };
+    if(shift < 0.25) {
+      f = f0
+    } else {
+      f = C(Math.cos(fr), Math.sin(fr)) * f0
+    };
     let ans = f;
     return {
       ...z,
@@ -70,7 +80,6 @@ function hypertile3(p, q, r, shift) {
     }
   }
 }
-
 
 const hypertile3Data = {
   name: "hypertile3",
--- standardLib.js
+++ standardLib.js
@@ -1,6 +1,9 @@
 /*helper functions*/
 function C(real, imaginary) {
-  return {re: real, im: imaginary};
+  return {
+    re: real,
+    im: imaginary
+  };
 }
 
 function div(z, c) {
@@ -52,7 +55,7 @@ function sqrt(z) {
   return multScalar({
     re: Math.sqrt(s + z.re),
     im: sgn * Math.sqrt(s - z.re)
-  }, 0.5 * Math.SQRT2 );
+  }, 0.5 * Math.SQRT2);
 }
 
 function log(z) {
@@ -79,8 +82,8 @@ function exp(z) {
 }
 
 /*Transforms*/
-function arcsinh(){
-  return function(z){
+function arcsinh() {
+  return function(z) {
     return {
       ...z,
       ...multScalar(
@@ -147,14 +150,97 @@ function rotate(theta) {
   }
 }
 
-function blurCircle(){
+function blurCircle() {
   return function(z) {
     let a = Math.random() * Math.PI * 2,
-        r = Math.sqrt(Math.random());
+      r = Math.sqrt(Math.random());
     let ans = {
       re: Math.cos(a) * r,
       im: Math.sin(a) * r
     }
+    return {
+      ...z,
+      ...ans
+    }
+  }
+}
+
+function hypertile3(p, q, r, shift) {
+  let rad = Math.pi / 180,
+    o = Math.acosh((Math.cos(Math.PI / p) + Math.cos(Math.PI / q) * Math.cos(Math.PI / r)) / (Math.sin(Math.PI / q) * Math.sin(Math.PI / r))),
+    a = Math.asinh(Math.sin(Math.PI / q) / Math.sin(Math.PI / p) * Math.sinh(o)),
+    b = Math.asinh(Math.sin(Math.PI / r) * Math.sinh(a)),
+    c = Math.asinh(Math.sin(Math.PI / r) * Math.sinh(a) / Math.sin(Math.PI / q)),
+    h = Math.tanh(b / 2),
+    b1 = Math.tanh(Math.acosh(Math.cosh(c) / Math.cosh(b)) / 2),
+    b2 = Math.tanh(Math.acosh(Math.cosh(a) / Math.cosh(b)) / 2),
+    rot1 = 360 / p * rad,
+    rot2 = 360 / q * rad,
+    rot3 = 360 / r * rad,
+    r01 = C(Math.cos(rot1), Math.sin(rot1)),
+    r02 = C(Math.cos(rot2), Math.sin(rot2)),
+    r03 = C(Math.cos(rot3), Math.sin(rot3));
+
+  const c1 = C(h, 0),
+    c2 = C(0, b1),
+    c3 = C(0, b2);
+
+  return function(z) {
+    let n, prf, z0;
+    if(shift < 0.25) {
+      n = 0
+      pfr = 0
+      z0 = z
+    } else if(shift < 0.5) {
+      n = p
+      pfr = rot1
+      z0 = div(add(z, c1), add(mult(c1, z), 1))
+    } else if(shift < 0.75) {
+      n = q
+      pfr = rot2
+      z0 = div(add(z, -c2), add(mult(c2, z), 1))
+    } else {
+      n = r
+      pfr = rot3
+      z0 = div(add(z, c3), add(mult(-c3, z), 1))
+    }
+
+    let m0 = div(add(z0, -c1), add(mult(-c1, z0), 1)),
+      r1 = r01 * m0,
+      m0f = div(add(r1, c1), add(mult(c1, r1), 1)),
+      m1 = div(add(z0, c2), add(mult(-c2, z0), 1)),
+      r2 = r02 * m1,
+      m1f = div(add(r2, -c2), add(mult(c2, r2), 1)),
+
+      m2 = div(add(z0, -c3), add(mult(c3, z0), 1)),
+      r3 = r03 * m2,
+      m2f = div(add(r3, c3), add(mult(-c3, r3), 1)),
+
+      fr = Math.floor(Math.random() * n) * pfr,
+      rnd = Math.random(),
+      f3, f0, f;
+    if(rnd < 1 / 3) {
+      f3 = m0f
+    } else if(rnd < 2 / 3) {
+      f3 = m1f
+    } else {
+      f3 = m2f
+    };
+    if(shift < 0.25) {
+      f0 = f3
+    } else if(shift < 0.5) {
+      f0 = div(add(f3, -c1), add(mult(-c1, f3), 1))
+    } else if(shift < 0.75) {
+      f0 = div(add(f3, c2), add(mult(-c2, f3), 1))
+    } else {
+      f0 = div(add(f3, -c3), add(mult(c3, f3), 1))
+    };
+    if(shift < 0.25) {
+      f = f0
+    } else {
+      f = C(Math.cos(fr), Math.sin(fr)) * f0
+    };
+    let ans = f;
     return {
       ...z,
       ...ans
@@ -170,69 +256,97 @@ const BUILT_IN_TRANSFORMS = {
   translate: translate,
   rotate: rotate,
   blurCircle: blurCircle,
+  hypertile3: hypertile3
 };
 
 const BUILT_IN_TRANSFORMS_PARAMS = {
-  arcsinh: [],
-  splits: [
-    {
-      name: "x",
-      type: "number",
-      default: 0
-    },
-    {
-      name: "y",
-      type: "number",
-      default: 0
-    }
-  ],
-  mobius: [
-    {
-      name: "a",
-      type: "complex",
-      default: {re:1,im:0}
-    },
-    {
-      name: "b",
-      type: "complex",
-      default: {re:0,im:0}
-    },
-    {
-      name: "c",
-      type: "complex",
-      default: {re:0,im:0}
-    },
-    {
-      name: "d",
-      type: "complex",
-      default: {re:1,im:0}
-    }
-  ],
-  scale: [
-    {
+    arcsinh: [],
+    splits: [{
+        name: "x",
+        type: "number",
+        default: 0
+      },
+      {
+        name: "y",
+        type: "number",
+        default: 0
+      }
+    ],
+    mobius: [{
+        name: "a",
+        type: "complex",
+        default: {
+          re: 1,
+          im: 0
+        }
+      },
+      {
+        name: "b",
+        type: "complex",
+        default: {
+          re: 0,
+          im: 0
+        }
+      },
+      {
+        name: "c",
+        type: "complex",
+        default: {
+          re: 0,
+          im: 0
+        }
+      },
+      {
+        name: "d",
+        type: "complex",
+        default: {
+          re: 1,
+          im: 0
+        }
+      }
+    ],
+    scale: [{
       name: "s",
       type: "number",
       default: 1
-    }
-  ],
-  translate: [
-    {
-      name: "real",
-      type: "number",
-      default: 0
-    },
-    {
-      name: "imaginary",
-      type: "number",
-      default: 0
-    }
-  ],
-  rotate: [
-    {
+    }],
+    translate: [{
+        name: "real",
+        type: "number",
+        default: 0
+      },
+      {
+        name: "imaginary",
+        type: "number",
+        default: 0
+      }
+    ],
+    rotate: [{
       name: "theta",
       type: "number",
       default: 0
-    }
-  ],
-  blurCircle: []
-};
+    }],
+    blurCircle: [],
+    hypertile3: [
+      {
+        name: "p",
+        type: "number",
+        default: 3
+      },
+      {
+        name: "q",
+        type: "number",
+        default: 3
+      },
+      {
+        name: "r",
+        type: "number",
+        default: 4
+      },
+      {
+        name: "d",
+        type: "number",
+        default: 0.5
+      }
+    ]
+  };
