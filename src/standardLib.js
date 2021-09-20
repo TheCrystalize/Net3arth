23e4f46860b84f6029bebccefb19a4ffbc9e271b
Crystalize <Smilezofficial@gmail.com>
2021-09-19 21:17:09 -0700

    + abs

--- src/standardLib.js
+++ src/standardLib.js
@@ -172,7 +172,7 @@ function blurCircle() {
 function blurGasket() {
   return z => {
     let a = Math.random() * Math.PI * 2,
-      r = 1 / Math.sqrt(Math.random() - Math.random());
+      r = 1 / Math.sqrt(Math.abs(Math.random() - Math.random()));
     return {
       ...z,
       re: Math.random() - 0.5,
